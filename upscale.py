c06321bed27f267a03eccc1d7ba784bf8f3d76c5
Trương Anh Hiếu <91637781+parallelism623@users.noreply.github.com>
2023-07-26 14:06:19 +0700

    Update upscale.py
    Signed-off-by: Trương Anh Hiếu <91637781+parallelism623@users.noreply.github.com>

--- upscale.py
+++ upscale.py
@@ -1,11 +1,12 @@
 import cv2
 from cv2 import dnn_superres
-sr = dnn_superres.DnnSuperResImpl_create()
-image = cv2.imread('000000001298.png')
-print(image.shape)
-path = "ESPCN_x3.pb"
-sr.readModel(path)
-sr.setModel("espcn", 3)
-result = sr.upsample(image)
-# Save the image
-cv2.imwrite("upscaled.png", result)
+def up_scale_x3(path_image, save_path):
+    sr = dnn_superres.DnnSuperResImpl_create()
+    image = cv2.imread(path_image)
+    print(image.shape)
+    path = "ESPCN_x3.pb"
+    sr.readModel(path)
+    sr.setModel("espcn", 3)
+    result = sr.upsample(image)
+    # Save the image
+    cv2.imwrite(save_path, result)
